7fc1ff98f4493ecb0fd2bc1a15c8d48e9a8bacbd
toucan-v8 <toucan305@gmail.com>
2025-06-03 19:41:45 +0300

    update data

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,4 @@
-//export const SUPPORT_EMAIL = "zakaz@tehtelecom.ru";
-export const SUPPORT_EMAIL = "toucan305@gmail.com";
+export const SUPPORT_EMAIL = "zakaz@tehtelecom.ru";
 export const SUPPORT_EMAIL_MAILTO = `mailto:${SUPPORT_EMAIL}`;
 
 export const SUPPORT_PHONE = "+7 (977) 953 39 11";
