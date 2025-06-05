44acfbe153f115f96ec36af5243b16fcba18a2be
blackchestnut <alexander.kalinichev@gmail.com>
2025-06-05 22:25:04 +0300

    Change github workflows

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
         },
       ],
     },
-    baseURL: process.env.NODE_ENV === "production" ? "/hotbox2/" : "/",
+    baseURL: '/',
   },
   site: {
     url: "https://hotbox2.igor.kalinichev.net",
